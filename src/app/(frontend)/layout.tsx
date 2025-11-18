1e1d9456d9141da95fb7520253e8594a13e0bfbd
karol.barkowski <karol.barkowski@gmail.com>
2025-11-18 01:21:43 +0100

    layout w.i.p.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,14 +1,21 @@
 import './styles.css'
 import React from 'react'
-import { LayoutFooter } from '@/components/LayoutComponents/Footer'
-import { LayoutHeader } from '@/components/LayoutComponents/Header'
 import { AdminBar } from '@/components/Payload/AdminBar'
 import { LivePreviewListener } from '@/components/Payload/LivePreviewListener'
+import { Unna } from 'next/font/google'
+import { LayoutFooter } from './_components/Footer'
+import { LayoutHeader } from './_components/Header'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
+
+const _UnnaFont = Unna({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-unna',
+})
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
@@ -20,13 +27,15 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
 
-      <body className="min-h-screen bg-linear-to-br from-slate-900 via-slate-800 to-slate-900 text-slate-100 flex flex-col">
+      <body className={`min-h-screen bg-savoy-bg text-savoy-text flex flex-col font-main`}>
         <AdminBar />
         <LivePreviewListener />
 
-        <LayoutHeader />
-        <main className="flex flex-1 py-16">{children}</main>
-        <LayoutFooter />
+        <div className="container mx-auto">
+          <LayoutHeader />
+          <main className="flex flex-1 py-8">{children}</main>
+          <LayoutFooter />
+        </div>
       </body>
     </html>
   )
